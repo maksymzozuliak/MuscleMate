371264469f80a83f832f99f2ad78036beaeb2d50
maksymzozuliak <max1max2mega56@gmail.com>
2023-06-08 16:01:22 +0300

    Network configuration

--- app/src/main/java/com/zozuliak/musclemate/MainActivity.kt
+++ app/src/main/java/com/zozuliak/musclemate/MainActivity.kt
@@ -10,14 +10,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.zozuliak.musclemate.presentation.main_screen.MainScreen
 import com.zozuliak.musclemate.ui.theme.MuscleMateTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MuscleMateTheme {
-
+                MainScreen()
             }
         }
     }
