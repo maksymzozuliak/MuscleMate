5422a033c271691d45559fcf49530657a918bbe0
maksymzozuliak <max1max2mega56@gmail.com>
2023-06-13 12:55:17 +0300

    UI setting configuration

--- app/src/main/java/com/zozuliak/musclemate/presentation/main_screen/MainScreen.kt
+++ app/src/main/java/com/zozuliak/musclemate/presentation/main_screen/MainScreen.kt
@@ -1,2 +1,72 @@
 package com.zozuliak.musclemate.presentation.main_screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.zozuliak.musclemate.R
+import com.zozuliak.musclemate.presentation.main_screen.components.ProfileButton
+
+@Composable
+fun MainScreen(
+    //viewModel: MainScreenViewModel = hiltViewModel<MainScreenViewModel>()
+) {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background),
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(
+                    start = 24.dp,
+                    end = 24.dp,
+                    top = 24.dp,
+                    bottom = 12.dp
+                ),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                modifier = Modifier
+                    .weight(1f),
+                text = "Workouts",
+                style = MaterialTheme.typography.titleLarge
+            )
+            ProfileButton(
+                modifier = Modifier
+                    .size(36.dp),
+                onClick = {},
+                color = MaterialTheme.colorScheme.tertiary
+            )
+        }
+
+    }
+}
+
+@Preview
+@Composable
+fun MainScreenPreview() {
+    Surface(modifier = Modifier.background(Color.White)) {
+        MainScreen()
+    }
+}
--- app/src/main/java/com/zozuliak/musclemate/ui/theme/Type.kt
+++ app/src/main/java/com/zozuliak/musclemate/ui/theme/Type.kt
@@ -1,19 +1,39 @@
 package com.zozuliak.musclemate.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.res.fontResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.zozuliak.musclemate.R
 
+
+val geologicaFontFamily = FontFamily(
+    Font(R.font.geologica_black),
+    Font(R.font.geologica_bold, weight = FontWeight.Bold),
+    Font(R.font.geologica_extrabold, weight = FontWeight.ExtraBold),
+    Font(R.font.geologica_extralight, weight = FontWeight.ExtraLight),
+    Font(R.font.geologica_light, weight = FontWeight.Light),
+    Font(R.font.geologica_medium, weight = FontWeight.Medium),
+    Font(R.font.geologica_regular, weight = FontWeight.Normal),
+    Font(R.font.geologica_semibold, weight = FontWeight.SemiBold),
+    Font(R.font.geologica_thin, weight = FontWeight.Thin)
+)
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = geologicaFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = geologicaFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp
     )
     /* Other default text styles to override
     titleLarge = TextStyle(
